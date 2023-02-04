ff27bad9c45977bf9846da60a05e588f852f0a1d
Jakub <malinowski.jakub@yahoo.com>
2023-02-04 15:31:29 -0600

    Assignment Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+    const form = document.getElementById("create-task-form");
+    const list = document.getElementById("tasks");
+    form.addEventListener('submit', (event) =>{
+        event.preventDefault();
+        let task = document.getElementById("new-task-description");
+        createItem(task.value);
+        form.reset();
+    });
+
+    function createItem(listItem){
+        const newItem = document.createElement("li");
+        newItem.textContent = `${listItem}`;
+    
+
+    const aButton = document.createElement("button");
+    aButton.textContent = "X";
+    aButton.addEventListener("click", handleDelete);
+    newItem.appendChild(aButton);
+
+    list.appendChild(newItem);
+    }
+    function handleDelete(e) {
+        e.target.parentNode.remove();
+    }
 });
